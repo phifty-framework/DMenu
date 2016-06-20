dde7f1226590c25e4c20f608ec6ef9cbf0494c33
c9s <yoanlin93@gmail.com>
2016-06-20 14:49:26 +0800

    Fix UpdateOrdering action

--- Action/UpdateOrdering.php
+++ Action/UpdateOrdering.php
@@ -9,17 +9,13 @@ class UpdateOrdering extends \ActionKit\Action
     public function run()
     {
         $orderingList = json_decode($this->arg('json'));
-        $error = false;
         foreach ($orderingList as $orderingItem) {
             $item = new MenuItem();
             $item->find(intval($orderingItem->record));
             $ret = $item->update(['ordering' => $orderingItem->order]);
-            if ($ret) {
-                $error = true;
+            if ($ret->error) {
+                return $this->error('選單順序更新失敗');
             }
-        }
-        if ($error) {
-            return $this->error('選單順序更新失敗');
         }
         return $this->success( _('Menu order has been updated.') );
     }
